79dcfba89f109fb7db972c946309bc4e2413e773
Rashidov01 <avazbekrashidov6@gmail.com>
2021-12-19 22:04:28 +0500

    added next section

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
 // window design
-let elNavbar = document.querySelector(".navbar")
+var elNavbar = document.querySelector(".navbar");
+var elBtn = document.querySelector('.scroll-up-btn')
 
 window.addEventListener("scroll", function(){
     if(this.scrollY > 20){
         elNavbar.classList.add("sticky");
     }else{
         elNavbar.classList.remove("sticky");
+    };
+    if(this.scrollY > 500){
+        elBtn.classList.add("show");
+    }else{
+        elBtn.classList.remove("show");
     }
-})
+});
 
 // typed.js
 var typed = new Typed(".type", {
     strings: [ "Frontend developer","Freelancer","UI designer"],
-    typeSpeed: 120,
-    backSpeed: 80,
+    typeSpeed: 160,
+    backSpeed: 120,
     loop: true
 });
 
 var typed = new Typed(".type__second", {
     strings: [ "Frontend developer","Freelancer","UI designer"],
-    typeSpeed: 120,
-    backSpeed: 80,
+    typeSpeed: 160,
+    backSpeed: 120,
     loop: true
+});
+
+//scroll-up
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
 });
