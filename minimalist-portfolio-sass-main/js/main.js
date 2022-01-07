cde252540fc0a063f0e87368b76cf183f9689d95
Abdulloh <avazbekrashidov6@gmail.com>
2022-01-07 16:46:24 +0500

    all done

--- minimalist-portfolio-sass-main/js/main.js
+++
@@ -1,6 +0,0 @@
-let elHeaderMenu = document.querySelector(".header");
-let elHamburgMenu = elHeaderMenu.querySelector(".header-hamburg__btn");
-
-elHamburgMenu.addEventListener("click", function(){
-  elHeaderMenu.classList.toggle("header-top__open");
-})
